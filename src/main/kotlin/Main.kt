156faea3019bdae0798d7e6e473ca31f240296de
Daniil Dryzhov <danildryzov@gmail.com>
2024-11-11 16:15:27 +0100

    first app

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package mate.academy
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println("Enter your city…")
+    val city = readLine()
+    println("User lives in $city")
+}
